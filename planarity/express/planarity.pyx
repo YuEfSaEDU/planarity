1338f5574ad159d78b35c7b910276f52700f4365
john-boyer-phd <john.boyer.phd@gmail.com>
2026-09-22 11:19:10 -0700

    Test pad_inches adjustment for Python 3.10 (not that we need it for much longer).

--- planarity/express/planarity.pyx
+++ planarity/express/planarity.pyx
@@ -564,6 +564,13 @@ cdef class PGraph:
                 "dependencies from Matplotlib."
             ) from matplotlib_import_error
 
+        try:
+            import sys
+        except ImportError as sys_import_error:
+            raise ImportError(
+                "planarity: draw() method failed, unable to import sys."
+            ) from sys_import_error
+
         ##################################################
         # Get the graphic rendering parameters from kwargs
         ##################################################
@@ -596,7 +603,11 @@ cdef class PGraph:
         plt.clf()
 
         fig = plt.figure(figsize=figsize, dpi=dpi, layout='constrained')
-        fig.get_layout_engine().set(w_pad=pad_inches, h_pad=pad_inches, hspace=0, wspace=0)
+
+        w_pad = float(pad_inches) if sys.version_info < (3, 11) else pad_inches
+        h_pad = float(pad_inches) if sys.version_info < (3, 11) else pad_inches
+        fig.get_layout_engine().set(w_pad=w_pad, h_pad=h_pad, hspace=0, wspace=0)
+
         fig.set_facecolor(kwargs.get('facecolor', '#ffffff'))
 
         ##################################
